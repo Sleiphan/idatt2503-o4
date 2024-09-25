89277bbd94264001302b3def08f2201b86ae5524
Håkon F. Fjellanger <hfredrif@gmail.com>
2024-09-25 09:02:58 +0000

    Complete Rust part of exercise 4

--- Exercise_4/src/main.rs
+++ Exercise_4/src/main.rs
@@ -1,5 +1,30 @@
+
+fn string_to_html(source: &str) -> String {
+    source.replace('&', "&amp;")
+          .replace('<', "&lt;")
+          .replace('>', "&gt;")
+}
+
+fn test_conversion(input: &str, expected: &str) -> bool {
+    expected == string_to_html(input)
+}
+
+struct TestSample {
+    input: String,
+    expected: String,
+}
+
 fn main() {
     println!("Hello, world!");
 
+    let samples: [TestSample; 3] = [
+        TestSample{input: String::from("Buns&Bozinos"), expected: String::from("Buns&amp;Bozinos")},
+        TestSample{input: String::from("1<2"), expected: String::from("1&lt;2")},
+        TestSample{input: String::from("cake>vegetables"), expected: String::from("cake&gt;vegetables")}
+        ];
     
+    for (index, sample) in samples.iter().enumerate() {
+        let equal: bool = test_conversion(&sample.input, &sample.expected);
+        println!("Test {}: {} \"{}\" -> \"{}\"", index, if equal {"success"} else {"FAIL"}, sample.input, sample.expected);
+    }
 }
