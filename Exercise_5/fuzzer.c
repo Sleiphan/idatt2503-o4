ba914cd5024e3f5f943db350d0f8dc7d2c8a871d
Haakon F. Fjellanger <hfredrif@gmail.com>
2024-10-04 13:54:23 +0000

    Complete E5

--- Exercise_5/fuzzer.c
+++ Exercise_5/fuzzer.c
@@ -5,14 +5,14 @@
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
-  // size_t buffer_size = size * 2;
-  // char *str = (char *)malloc(sizeof(char) * buffer_size);
-  // memcpy(str, data, size);
-  // str[size] = '\0';
+  size_t buffer_size = size * 2;
+  char *str = (char *)malloc(sizeof(char) * buffer_size);
+  memcpy(str, data, size);
+  str[size] = '\0';
 
-  // string_to_html(str, size + 1, buffer_size);
+  string_to_html(str, size + 1, buffer_size);
 
-  // free(str);
+  free(str);
 
   return 0;
 }
